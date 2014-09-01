f6491a140e744d2e8fd45b2d26ed0ee6dbfaa36e
EsCaliente <e.damisch42@gmail.com>
2014-08-31 23:45:24 -0500

    Changed UI Tab Bar Item Color
    That’s It

---
+++ Commutable/Commutable/CommutableTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  CommutableTabBarController.h
+//  Commutable
+//
+//  Created by Edward Damisch on 8/31/14.
+//  Copyright (c) 2014 Commutable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommutableTabBarController : UITabBarController
+
+@end
---
+++ Commutable/Commutable/CommutableTabBarController.m
@@ -0,0 +1,87 @@
+//
+//  CommutableTabBarController.m
+//  Commutable
+//
+//  Created by Edward Damisch on 8/31/14.
+//  Copyright (c) 2014 Commutable. All rights reserved.
+//
+
+#import "CommutableTabBarController.h"
+
+@interface CommutableTabBarController ()
+-(UIColor*)colorWithHexString:(NSString*)hex;
+@end
+
+@implementation CommutableTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [[UITabBar appearance] setTintColor:[self colorWithHexString:@"3399CC"]];
+}
+
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
